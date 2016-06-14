f07ed9cac2748c6f57192824b2cde6f3a82d237c
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2016-06-14 13:38:17 +0100

    Add store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import *  as actions from './actions';
 import Stream from './components/Stream';
@@ -17,6 +18,8 @@ const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream />,
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
   document.getElementById('app')
 );
