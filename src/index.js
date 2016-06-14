bf8cb7552f8f32833801c78893059f41a72de128
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2016-06-14 11:34:13 +0100

    Second iteration done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('My Soundcloud App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+ReactDOM.render(
+  <Stream tracks={tracks} />,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
